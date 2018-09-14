f93d76f3ffbd7014f599eb30ad5f291419291c0a
Adrienn Eva Csengeri-Pap <eva@zzish.com>
2018-09-14 11:33:53 +0100

    Fixing babel for class variables.

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ var config = {
     module: {
         loaders: [
             {
-                test: /\.jsx?/,
+                test: /\.(js|jsx)$/,
                 include: APP_DIR,
                 loader: "babel-loader",
             },
